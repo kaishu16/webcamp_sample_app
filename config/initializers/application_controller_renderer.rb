3cde4d444f2803c3e1429641ac179fdd915a9eed
kaishu16 <shimizu.kaishu.infratop@gmail.com>
2019-03-06 23:43:03 +0900

    Initial Commit Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'a4338424766f9a4b225313cec0f5044c5f959da4f1e3eb1cdbe32b19b7794811ac809fe40bcdc9d81270877cd5b5c190e28b1f66691d976a3827ac43d97b36ec'
